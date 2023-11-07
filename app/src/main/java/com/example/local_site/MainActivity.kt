ab9b1111b0e5624dd0858c5ebfde3051a0751c67
3126058535 <3126058535@qq.com>
2023-11-07 16:41:36 +0800

    this is first commit

--- app/src/main/java/com/example/local_site/MainActivity.kt
+++ app/src/main/java/com/example/local_site/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.local_site.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,11 @@ class MainActivity : AppCompatActivity() {
         val Button = findViewById<Button>(R.id.button2)
         Button.setOnClickListener{
             val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+        val Button1 = findViewById<Button>(R.id.button1)
+        Button1.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/local_site/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.example.local_site.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.local_site.data.LoginDataSource
+import com.example.local_site.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
